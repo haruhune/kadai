11a5179344b1fd72ff59129b6dadc459aaf1ae5a
takahashi riki <r.takahashi.sys20@morijyobi.ac.jp>
2021-03-30 12:59:57 +0900

    修正しました

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import calc_bmi
 
 print("bmi計算アプリ")
-w = int(input("体重は:"))
-h = int(input("身長は:"))
+w = int(input("体重は?:"))
+h = int(input("身長は?:"))
 print(f"BMI:{calc_bmi.get_bmi(w,h)}")
 print(f"BMI:{calc_bmi.get_bmi(w,h)}")
